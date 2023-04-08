676d33b621a7567e5f01bddc66b37d07dba6259e
MinJee Kim <hbmj1017@naver.com>
2023-04-08 14:36:12 +0900

    Modify Algorithm-Test-04

--- Algorithm-Test-04/EXAM-01/2200098/간단한 N의 약수.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdin = open('input_N의 약수.txt', 'r')
-
-N = int(input())
-list_N = []
-for n in range(1,N+1) :
-    if N % n == 0 :
-        list_N.append(n)
-print(*list_N)
--- Algorithm-Test-04/kdt-02-career/list/List_companyResearchSite.md
+++
@@ -1,9 +0,0 @@
-# 기업분석 참고 리스트
-
-## [the VC](https://thevc.kr/)
-
-사이트 특징
-
-## [혁신의 숲](https://www.innoforest.co.kr/)
-
-사이트 특징
